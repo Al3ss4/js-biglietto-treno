d5c80b71a326fccb5f984183f7fa784bd8ddf879
Mario Rovito <alessandro.r14@gmail.com>
2022-03-07 09:28:59 +0100

    titolo corretto

---
+++ script.js
@@ -0,0 +1,29 @@
+console.log('JS ok')
+
+// calcolo km
+const kmDaPercorrere = prompt('Quanti km vuoi percorrere?');
+document.getElementById('km').innerHTML = 'km da percorrere:' + kmDaPercorrere;
+
+//calcolo età
+const età = prompt('Quanti anni hai?');
+document.getElementById ('anni').innerHTML = 'anni:' + età;
+
+// alert inserire km
+if(kmDaPercorrere <= 0){
+    alert('Si prega di inserire correttamente il numero di km');
+}
+
+ // calcolo prezzo del biglietto
+ var prezzoBiglietto = 0.21 * kmDaPercorrere;
+ var scontistica= 'Nessuno Sconto';
+
+ if(età < 18){
+    prezzoBiglietto = (prezzoBiglietto * 0.2).toFixed(2);
+    scontistica = 'Sconto Minorenne';
+} else if(età > 65){
+    prezzoBiglietto = (prezzoBiglietto * 0.4).toFixed(2);
+    scontistica = 'Sconto Over 65';
+}
+
+document.getElementById('offerta').innerHTML = scontistica;
+document.getElementById('costo-biglietto').innerHTML = prezzoBiglietto + '€'; 
